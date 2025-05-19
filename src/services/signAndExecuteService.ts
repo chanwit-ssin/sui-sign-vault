bbc109345778913924f87b53eff20997e74c9d3c
kris <kris@finema.co>
2025-05-19 22:48:18 +0700

    fix package

--- src/services/signAndExecuteService.ts
+++ src/services/signAndExecuteService.ts
@@ -3,11 +3,6 @@ import { Transaction } from "@mysten/sui/transactions";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 import { PACKAGE_ID } from "@/config/constants";
 
-export function useSignAndExecuteService() {
-  const rpcUrl = getFullnodeUrl("testnet");
-  const client = new SuiClient({ url: rpcUrl });
-}
-
 export async function createAllowlist(
   name: string,
   wallet: any
@@ -18,13 +13,20 @@ export async function createAllowlist(
   if (!name) throw new Error("Name is required");
   const tx = new Transaction();
 
+
   tx.moveCall({
-    target: `${PACKAGE_ID}::allowlist::create_allowlist_entry`,
+    target: `0x4cb081457b1e098d566a277f605ba48410e26e66eaab5b3be4f6c560e9501800::allowlist::create_allowlist_entry`,
     arguments: [tx.pure.string(name)],
   });
-  tx.setGasBudget(10_000_000);
+
+  tx.setGasBudget(10000000);
+
   const result = await wallet.signAndExecuteTransaction({
     transaction: tx,
+    options: {
+      showRawEffects: true,
+      showEffects: true,
+    },
   });
   // const allowlistObj = result.effects?.created?.find(
   //   (o) => o.owner && typeof o.owner === "object" && "Shared" in o.owner
